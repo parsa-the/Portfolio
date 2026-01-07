17be3b32de9745d6bf5ac2244c38b2557bdd6195
parsa_the <parsaparvizi018@gmail.com>
2026-01-07 18:32:49 +0330

    remove chat app

--- src/app/component/ui/Card.tsx
+++ src/app/component/ui/Card.tsx
@@ -29,21 +29,21 @@ export const Projects = [
       "my portfolio with stunning design and animations with framer and tailwind and email js for the contact form",
     url: "https://portfolio-kappa-mauve-91.vercel.app",
   },
-  {
-    image: "/projects/chat-app.png",
-    name: "Chat-app",
-    techstack: [
-      "next.js",
-      "Typescript",
-      "framer motion",
-      "supabase",
-      "tailwind",
-      "vercel",
-    ],
-    discription:
-      "realtime & fast chat application made using supabse for backend and next js for frontend ",
-    url: "https://chat-app-gules-six-85.vercel.app",
-  },
+  // {
+  //   image: "/projects/chat-app.png",
+  //   name: "Chat-app",
+  //   techstack: [
+  //     "next.js",
+  //     "Typescript",
+  //     "framer motion",
+  //     "supabase",
+  //     "tailwind",
+  //     "vercel",
+  //   ],
+  //   discription:
+  //     "realtime & fast chat application made using supabse for backend and next js for frontend ",
+  //   url: "https://chat-app-gules-six-85.vercel.app",
+  // },
 ];
 
 
